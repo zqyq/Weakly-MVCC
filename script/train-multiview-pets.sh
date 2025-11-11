07868e75dc74ae48064ea77a37c6057a728a4d9e
hereislxb <149448780+hereislxb@users.noreply.github.com>
2025-11-11 14:59:54 +0800

    Add files via upload

---
+++ script/train-multiview-pets.sh
@@ -0,0 +1,1 @@
+python main_pets_ll.py --multi_view_train --cuda_order 1 --ckpt ../calibrationFree/logs/pets_frame/pets/debug/2025-05-24_08-03-23/models/Ep_25_mae_3.863309015411338_mse_26.575469027983267.pth
---
+++ script/train-singleview.sh
@@ -0,0 +1,1 @@
+python main_pets_ll.py --onlyTrainSingleView  --cuda_order 1
